0010d90889b24e1aa98156d709cfe736462be95b
Calin Ipate <calinipate@gmail.com>
2017-11-17 23:27:47 +0200

    fix on recent comments panel

--- bootstrap4-widgets.php
+++ bootstrap4-widgets.php
@@ -11,102 +11,102 @@
 
 class bootstrap4_base_widget extends WP_Widget {
 
-    public $text_color = "";
-    public $class = "";
-    public $badge_class = "";
-    protected $classes = [
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark',
-    ];
+	public $text_color = "";
+	public $class = "";
+	public $badge_class = "";
+	protected $classes = [
+		'primary',
+		'secondary',
+		'success',
+		'danger',
+		'warning',
+		'info',
+		'light',
+		'dark',
+	];
 
-    function widget($args, $instance) {
-        extract($args);
-        $this->class = "bg-" . $instance['class'];
-        $this->badge_class = "badge-" . $instance['class'];
-        $this->text_color = "text-white";
+	function widget( $args, $instance ) {
+		extract( $args );
+		$this->class       = "bg-" . $instance['class'];
+		$this->badge_class = "badge-" . $instance['class'];
+		$this->text_color  = "text-white";
 
-        if ($this->class == "bg-light" || $this->class == "bg-warning") {
-            $this->text_color = "";
-        }
-    }
+		if ( $this->class == "bg-light" || $this->class == "bg-warning" ) {
+			$this->text_color = "";
+		}
+	}
 
 }
 
 class WidgetStyle {
 
-    public $class = "";
-    public $cardClass = "";
-    public $badgeClass = "";
-    public $headerTextColor = "text-white";
-    public $listGroupColor = "";
+	public $class = "";
+	public $cardClass = "";
+	public $badgeClass = "";
+	public $headerTextColor = "text-white";
+	public $listGroupColor = "";
 
-    function __construct($class) {
-        $this->class = $class;
-        $this->cardClass = "bg-" . $this->class;
-        $this->badgeClass = "badge-" . $this->class;
-        if ($class == "light" || $class == "warning") {
-            $this->headerTextColor = "";
-        }
-        $this->listGroupColor = "list-group-item-" . $class;
-    }
+	function __construct( $class ) {
+		$this->class      = $class;
+		$this->cardClass  = "bg-" . $this->class;
+		$this->badgeClass = "badge-" . $this->class;
+		if ( $class == "light" || $class == "warning" ) {
+			$this->headerTextColor = "";
+		}
+		$this->listGroupColor = "list-group-item-" . $class;
+	}
 
-    /**
-     * This method returns the class to be used for Bootstrap Cards
-     * @return string
-     */
-    public function getCardClass() {
+	/**
+	 * This method returns the class to be used for Bootstrap Cards
+	 * @return string
+	 */
+	public function getCardClass() {
 
-        return $this->cardClass;
-    }
+		return $this->cardClass;
+	}
 
 	/**
 	 * This method returns the badge class to be used in Bootstrap Cards
 	 * @return string
 	 */
-    public function getBadgeClass() {
+	public function getBadgeClass() {
 
-        return $this->badgeClass;
-    }
+		return $this->badgeClass;
+	}
 
 	/**
 	 * This method returns the text to be used in the Bootstrap Cards header
 	 * @return string
 	 */
-    public function getHeaderTextColor() {
+	public function getHeaderTextColor() {
 
-        return $this->headerTextColor;
-    }
+		return $this->headerTextColor;
+	}
 
 	/**
 	 * This method returns the list items class used in Bootstrap Cards
 	 * @return string
 	 */
-    public function getListItemClass() {
+	public function getListItemClass() {
 
-        return $this->listGroupColor;
-    }
+		return $this->listGroupColor;
+	}
 
 }
 
-include(plugin_dir_path(__FILE__) . 'panels/list-categories-widget.php');
-include(plugin_dir_path(__FILE__) . 'panels/list-recent-posts-widget.php');
-include(plugin_dir_path(__FILE__) . 'panels/list-recent-comments-widget.php');
-include(plugin_dir_path(__FILE__) . 'panels/list-archives-widget.php');
+include( plugin_dir_path( __FILE__ ) . 'panels/list-categories-widget.php' );
+include( plugin_dir_path( __FILE__ ) . 'panels/list-recent-posts-widget.php' );
+include( plugin_dir_path( __FILE__ ) . 'panels/list-recent-comments-widget.php' );
+include( plugin_dir_path( __FILE__ ) . 'panels/list-archives-widget.php' );
 
 function bootstrap4_widgets_register_widgets() {
-    register_widget('bootstrap4_list_categories_widget');
-    register_widget('bootstrap4_recent_posts_widget');
-    register_widget('bootstrap4_recent_comments_widget');
-    register_widget('bootstrap4_list_archives_widget');
+	register_widget( 'bootstrap4_list_categories_widget' );
+	register_widget( 'bootstrap4_recent_posts_widget' );
+	register_widget( 'bootstrap4_recent_comments_widget' );
+	register_widget( 'bootstrap4_list_archives_widget' );
 }
 
-add_action('widgets_init', 'bootstrap4_widgets_register_widgets');
+add_action( 'widgets_init', 'bootstrap4_widgets_register_widgets' );
 
 // Add this to enqueue your scripts on Page Builder too
-add_action('siteorigin_panel_enqueue_admin_scripts', 'bootstrap4_widgets_register_widgets');
+add_action( 'siteorigin_panel_enqueue_admin_scripts', 'bootstrap4_widgets_register_widgets' );
--- panels/list-recent-comments-widget.php
+++ panels/list-recent-comments-widget.php
@@ -27,21 +27,21 @@ class bootstrap4_recent_comments_widget extends bootstrap4_base_widget {
         ?>
         <div class="card <?php echo $widgetStyle->getCardClass(); ?> mb-3">
             <div class="card-header <?php echo $widgetStyle->getHeaderTextColor(); ?>"><?php echo $title ?></div>
-            <ul class="list-group list-group-flush">
+            <div class="list-group list-group-flush">
                 <?php
                 if (is_array($comments) && $comments) {
                     foreach ((array) $comments as $comment) {
-                        $output = '<li class="list-group-item ' . $widgetStyle->getListItemClass() . '">';
+                        $output = '<div class="list-group-item ' . $widgetStyle->getListItemClass() . '">';
                         /* translators: comments widget: 1: comment author, 2: post link */
                         $output .= sprintf(_x('%1$s on %2$s', 'widgets'), '<span class="comment-author-link">' . get_comment_author_link($comment) . '</span>', '<a href="' . esc_url(get_comment_link($comment)) . '">' . get_the_title($comment->comment_post_ID) . '</a>'
                         );
-                        $output .= '</li>';
+                        $output .= '</div>';
 
                         echo $output;
                     }
                 }
                 ?>
-            </ul>
+            </div>
         </div>
         <?php
     }
